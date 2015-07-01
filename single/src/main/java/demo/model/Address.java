cf15acc9c418dd9ecebba26c4fdd5efe0218dc2c
nikhil-thakur <nikhil.thakur.java@gmail.com>
2015-06-30 23:00:20 -0400

    field renamed in Address.java

--- single/src/main/java/demo/model/Address.java
+++ single/src/main/java/demo/model/Address.java
@@ -4,19 +4,19 @@ package demo.model;
  * Created by nikhi_000 on 6/30/2015.
  */
 public class Address {
-    private String flastNumber;
+    private String flatNumber;
     private String apartmentName;
     private String areaName;
     private String city;
     private String zip;
     private String country;
 
-    public String getFlastNumber() {
-        return flastNumber;
+    public String getFlatNumber() {
+        return flatNumber;
     }
 
-    public void setFlastNumber(String flastNumber) {
-        this.flastNumber = flastNumber;
+    public void setFlatNumber(String flatNumber) {
+        this.flatNumber = flatNumber;
     }
 
     public String getApartmentName() {
@@ -66,7 +66,7 @@ public class Address {
 
         Address address = (Address) o;
 
-        if (getFlastNumber() != null ? !getFlastNumber().equals(address.getFlastNumber()) : address.getFlastNumber() != null)
+        if (getFlatNumber() != null ? !getFlatNumber().equals(address.getFlatNumber()) : address.getFlatNumber() != null)
             return false;
         if (getApartmentName() != null ? !getApartmentName().equals(address.getApartmentName()) : address.getApartmentName() != null)
             return false;
@@ -79,7 +79,7 @@ public class Address {
 
     @Override
     public int hashCode() {
-        int result = getFlastNumber() != null ? getFlastNumber().hashCode() : 0;
+        int result = getFlatNumber() != null ? getFlatNumber().hashCode() : 0;
         result = 31 * result + (getApartmentName() != null ? getApartmentName().hashCode() : 0);
         result = 31 * result + getAreaName().hashCode();
         result = 31 * result + getCity().hashCode();
@@ -91,7 +91,7 @@ public class Address {
     @Override
     public String toString() {
         return "Address{" +
-                "flastNumber='" + flastNumber + '\'' +
+                "flatNumber='" + flatNumber + '\'' +
                 ", apartmentName='" + apartmentName + '\'' +
                 ", areaName='" + areaName + '\'' +
                 ", city='" + city + '\'' +
